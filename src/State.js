bd89680f3ec70a35436bb64908be86531d85f9f1
kamenifrancis <blickson57@gmail.com>
2023-02-09 01:34:33 +0100

    this.state dans react

---
+++ src/State.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+    
+class Etat extends Component { 
+  constructor(props){ 
+    super(props) 
+        
+    // Set initial state 
+    this.state = {greeting : 
+          'Click the button to receive greetings'} 
+        
+    // Binding this keyword 
+    this.updateState = this.updateState.bind(this) 
+  } 
+    
+  updateState(){ 
+    // Changing state 
+    this.setState({greeting : 
+                 'GeeksForGeeks welcomes you !!'}) 
+  } 
+      
+  render(){ 
+    return ( 
+      <div> 
+       <h2>Greetings Portal</h2>   
+       <p>{this.state.greeting}</p>
+      
+        {/* Set click handler */} 
+        <button onClick={this.updateState}> 
+          Click me!
+        </button> 
+      </div> 
+    ) 
+  } 
+} 
+    
+export default Etat
